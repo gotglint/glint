52e6fbf50f0d9d85d803b5ccf88e29e99fc60c1e
Michael Laccetti <michael@laccetti.com>
2016-08-27 11:53:40 -0400

    Removing unused files

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const hub = require('gulp-hub');
-
-const projects = {
-  client: 'glint-client/gulpfile.js',
-  server: 'glint-server/gulpfile.js'
-  dashboard: 'glint-dashboard/gulpfile.js'
-};
-
-hub([projects.client, projects.server, projects.dashboard]);
